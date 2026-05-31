f8310433b876c2999afedf80f5e279297b03f1e9
Mehedi <mehedi.interface@gmail.com>
2026-05-31 20:29:57 +0600

    Exercise - 1

--- exercise-1.js
+++ exercise-1.js
@@ -1,16 +1,19 @@
 /*
 Exercise 1: Positive or Negative
-
 Create a variable number.
-
-If the number is greater than 0, print "Positive".
-Otherwise, print "Negative or Zero".
+- If the number is greater than 0, print "Positive".
+- Otherwise, print "Negative or Zero".
 
 Example:
-
 let number = 5;
 
 Output:
-
 Positive
 */
+
+let number = 5; 
+if (number > 0) {
+    console.log("Positive"); 
+} else {
+    console.log("Negative"); 
+}
